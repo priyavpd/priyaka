acff579382a34aab3537239d2ed6166007f5f9a4
priyavpd <vpriyacse2@gmail.com>
2016-08-06 22:14:39 -0700

    Create ListOddNumbers.java

---
+++ ListOddNumbers.java
@@ -0,0 +1,20 @@
+# priyaka
+
+public class ListOddNumbers {
+ 
+        public static void main(String[] args) {
+               
+                //define limit
+                int limit = 50;
+               
+                System.out.println("Printing Even numbers between 1 and " + limit);
+               
+                for(int i=1; i <= limit; i++){
+                       
+                        // if the number is divisible by 2 then it is even
+                        if( i % 2 == 1){
+                                System.out.print(i + " ");
+                        }
+                }
+        }
+}
